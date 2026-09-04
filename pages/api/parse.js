d80049121bf9612e4844228ee16bb44ac4d7676a
ravilavanya362-creator <ravilavanya362@gmail.com>
2026-09-04 08:39:13 +0530

    Update parse.js

--- pages/api/parse.js
+++ pages/api/parse.js
@@ -31,7 +31,7 @@ async function resolveToCanonicalVideoUrl(inputUrl) {
 }
 
 export default async function handler(req, res) {
-  const { url } = req.query;
+  const url = req.method === 'POST' ? req.body?.url : req.query.url;
 
   if (!url) {
     return res.status(400).json({ error: 'URL is required' });
